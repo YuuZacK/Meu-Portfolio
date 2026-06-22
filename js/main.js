081c85569d2a0f24684670ce4cf008151f76423a
YuuZacK <isaqui.cb@gmail.com>
2026-06-21 23:56:30 -0300

    estruturação de pastas e isolamento js

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const words = ["Desenvolvedor Web", "Engenheiro de Software", "Técnico de Hardware"];
+let i = 0;
+let timer;
+
+function typingEffect() {
+    let word = words[i].split("");
+    var loopTyping = function () {
+        if (word.length > 0) {
+            document.getElementById('typing-text').innerHTML += word.shift();
+        } else {
+            setTimeout(deletingEffect, 2000);
+            return false;
+        }
+        timer = setTimeout(loopTyping, 100);
+    };
+    loopTyping();
+}
+
+function deletingEffect() {
+    let word = words[i].split("");
+    var loopDeleting = function () {
+        if (word.length > 0) {
+            word.pop();
+            document.getElementById('typing-text').innerHTML = word.join("");
+        } else {
+            if (words.length > (i + 1)) {
+                i++;
+            } else {
+                i = 0;
+            }
+            setTimeout(typingEffect, 500);
+            return false;
+        }
+        timer = setTimeout(loopDeleting, 50);
+    };
+    loopDeleting();
+}
+
+typingEffect();
